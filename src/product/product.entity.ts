c27b223fd90d8b9356c92c4e7ad6a2dfbe95a80d
victorsena19 <victor_sena19@hotmail.com>
2024-02-16 11:14:10 -0400

    feat: created entity product

---
+++ src/product/product.entity.ts
@@ -0,0 +1,55 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ProductImagesEntity } from './product-image.entity';
+import { ProductCharacteristicEntity } from './product-characteristic.entity';
+
+@Entity({ name: 'products' })
+export class ProductEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
+  name: string;
+
+  @Column({ name: 'value', nullable: false })
+  value: number;
+
+  @Column({ name: 'quantidade_disponivel', nullable: false })
+  quantidadeDisponivel: number;
+
+  @Column({ name: 'descricao', length: 255, nullable: false })
+  descricao: string;
+
+  @Column({ name: 'categoria', length: 100, nullable: false })
+  categoria: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+
+  @OneToMany(
+    () => ProductImagesEntity,
+    (productImagesEntity) => productImagesEntity.product,
+    { cascade: true, eager: true },
+  )
+  images: ProductImagesEntity[];
+
+  @OneToMany(
+    () => ProductCharacteristicEntity,
+    (productCharacteristicEntity) => productCharacteristicEntity.product,
+    { cascade: true, eager: true },
+  )
+  characteristic: ProductCharacteristicEntity[];
+}
