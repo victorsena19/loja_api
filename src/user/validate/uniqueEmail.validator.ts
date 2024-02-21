4e12169a18f0270bab58f4a51723cbc3fe29f4cb
victorsena19 <victor_sena19@hotmail.com>
2024-02-21 17:11:13 -0400

    feat: created validator uniqueEmail

---
+++ src/user/validate/uniqueEmail.validator.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import {
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  registerDecorator,
+} from 'class-validator';
+import { UserService } from '../user.service';
+@Injectable()
+@ValidatorConstraint({ async: true })
+export class UniqueEmailValidator implements ValidatorConstraintInterface {
+  constructor(private userService: UserService) {}
+
+  async validate(value: any): Promise<boolean> {
+    const userEmailExist = await this.userService.searchEmail(value);
+    return !userEmailExist;
+  }
+}
+
+export const UniqueEmail = (optionsValidator: ValidationOptions) => {
+  return (object: object, property: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: property,
+      options: optionsValidator,
+      constraints: [],
+      validator: UniqueEmailValidator,
+    });
+  };
+};
