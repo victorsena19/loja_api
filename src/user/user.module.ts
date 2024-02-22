8ccd9062e987951bc6cf4625966b52327b03e5f5
victorsena19 <victor_sena19@hotmail.com>
2024-02-22 10:13:44 -0400

    build: add controllers userController

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 import { UniqueEmailValidator } from './validate/uniqueEmail.validator';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UserController],
   providers: [UserService, UniqueEmailValidator],
 })
 export class UserModule {}
