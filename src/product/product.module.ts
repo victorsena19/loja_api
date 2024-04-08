acb077b68fbe5939e2c27610088d6266d6d8f8a5
victorsena19 <victor_sena19@hotmail.com>
2024-04-08 09:21:32 -0400

    feat: created modulo ProductModule

---
+++ src/product/product.module.ts
@@ -0,0 +1,12 @@
+import { Controller, Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './product.entity';
+import { ProductService } from './product.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
+export class ProductModule {}
