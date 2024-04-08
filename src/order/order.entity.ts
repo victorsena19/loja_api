d6c2ea37728f53e3765993102a603a8415d209a6
victorsena19 <victor_sena19@hotmail.com>
2024-04-08 09:32:42 -0400

    feat: created entity OrderEntity

---
+++ src/order/order.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { StatusPedido } from './enum/StatusPedido.enum';
+import { UserEntity } from '../user/user.entity';
+import { ItemOrderEntity } from './ItemOrder.entity';
+
+@Entity({ name: 'order' })
+export class OrderEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'amount', nullable: false })
+  amount: number;
+
+  @Column({ name: 'status', enum: StatusPedido, nullable: false })
+  status: StatusPedido;
+
+  @ManyToOne(() => UserEntity, (user) => user.order)
+  user: UserEntity;
+
+  @OneToMany(() => ItemOrderEntity, (itemOrder) => itemOrder.order, {
+    cascade: true,
+  })
+  itensOrder: ItemOrderEntity[];
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+}
