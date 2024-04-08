195a340bd29f993e3fb3df2aaf28e4108ff9055e
victorsena19 <victor_sena19@hotmail.com>
2024-04-08 09:16:55 -0400

    feat: created service ProductService

---
+++ src/product/product.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from './product.entity';
+import { Repository } from 'typeorm';
+import { CreateProductDTO } from './dto/createProductDTO';
+import { UpdateProductDTO } from './dto/updateProductDTO';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private readonly productRepository: Repository<ProductEntity>,
+  ) {}
+
+  async listProduct() {
+    const product = await this.productRepository.find();
+    return product;
+  }
+
+  async createProduct(product: CreateProductDTO) {
+    const productEntity = new ProductEntity();
+
+    productEntity.name = product.name;
+    productEntity.value = product.value;
+    productEntity.availableQuantity = product.availableQuantity;
+    productEntity.description = product.description;
+    productEntity.category = product.category;
+    productEntity.characteristics = product.characteristics;
+    productEntity.images = product.images;
+
+    return await this.productRepository.save(productEntity);
+  }
+
+  async updateProduct(id: string, newProduct: UpdateProductDTO) {
+    const productId = await this.productRepository.findOneBy({ id });
+    Object.assign(productId, newProduct);
+    return this.productRepository.save(productId);
+  }
+
+  async deleteProduct(id: string) {
+    await this.productRepository.delete(id);
+  }
+}
